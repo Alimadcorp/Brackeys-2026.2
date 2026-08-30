b7421cbe99b9f474879be964e5d318498559d34b
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-08-30 16:46:05 +0500

    meow

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play() {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
